c7f98e61f14d51df890eb465fdfa30888d057a9a
EmiliaT-M <136384667+EmiliaT-M@users.noreply.github.com>
2025-03-12 23:03:08 -0600

    feat:Anexo de base de datos

--- conexionSQL/conexionBD.py
+++ conexionSQL/conexionBD.py
@@ -1,3 +1,39 @@
+#Conexion base de datos
 import sqlite3
-con = sqlite3.connect("tutorial.db")
+
+#Indicamos la conexion
+con = sqlite3.connect("gestoBD.db")
+#Agregamos el cursor
+cursor=con.cursor()
+
+
+#Metodo que agrega un elemento a la BD
+def agregar_contrasena(usuario,contrasena, sitio):
+    cursor.execute(
+        """INSERT INTO contrasenia VALUES ('{usuario}','{contrasena}','{sitio}',{date})"""
+    )
+    con.commit()
+
+#metodo que elimina fila
+def eliminar_contrasena(usuario):
+    cursor.execute(
+       """DELETE FROM contrasenia WHERE usuario='{usuario}' """
+    )
+    con.commit()
+
+
+#Metodo que cambia el valor de la contrasena
+def cambiar_contrasena(sitio, contrasena):
+    cursor.execute(
+       """UPDATE FROM contrasenia SET contrasenia= '{contrasena}'WHERE sitio='{sitio}' """
+    )
+    con.commit()
+
+def cambiar_usuario(usuario, sitio):
+    cursor.execute(
+       """UPDATE FROM contrasenia SET usuario= '{usuario}'WHERE sitio='{sitio}' """
+    )
+    con.commit()
+    
+                
 
