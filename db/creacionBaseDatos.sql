8b60173ec146f8f2ec673c19d433d6abf618f244
EmiliaT-M <136384667+EmiliaT-M@users.noreply.github.com>
2025-03-18 14:35:24 -0600

    feat:Implementacion de comentarios

--- db/creacionBaseDatos.sql
+++ db/creacionBaseDatos.sql
@@ -1,5 +1,10 @@
---CREATE DATABASE  gestorBD;
+--Solo se ejecuta cuando la base de datos solo existe y no tiene tablas
+
 -- Crear la base de datos
+
+CREATE DATABASE  gestorBD;
+
+--Crear tablas de la base de datos
 CREATE TABLE contrasenias (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     usuario TEXT NOT NULL,
